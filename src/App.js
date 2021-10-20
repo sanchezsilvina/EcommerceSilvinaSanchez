82949beabb8f760ea4ae9334c39cb13a1598dd78
unknown <silvinaasanchez@gmail.com>
2021-10-20 14:00:38 -0300

    mensaje del commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import fondo from './suculentas.jpg';
 import './App.css';
+import NavBar from './componentes/navbar/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App container mt-8">
+      <div className="row mx-0 px-0 mt-4">
+        <div className="col-md-3 text-left"><h6 className="Fuente-Logo">Entre Verde</h6></div>
+         <div className="col-md-9 text-right"><NavBar/></div>
+      </div>  
+      <div className="row mx-0 px-0 mt-4">
+        <div className="col-md-12"><img className="App-Fondo" src={fondo} alt="Entre Verdes"/></div>
+      </div>
     </div>
   );
 }
